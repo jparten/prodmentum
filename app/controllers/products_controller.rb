480a7f25ad06dc978b9c462082b38e2d59dab15a
Jon Parten <jon.t.parten@nordstrom.com>
2015-05-06 15:41:41 -0700

    Adds basic product edit and show actions and views

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,23 @@
+class ProductsController < ApplicationController
+   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params) 
+    if @product.save 
+      redirect_to @product, notice: "Your product has been successfully created."
+    else 
+      render :new
+    end 
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+  def product_params
+      params.require(:product).permit(:name, :description)
+  end
+end

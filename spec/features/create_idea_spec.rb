8dcfa657c82bc521f5b81e4b60efb48af7b55617
Jon Parten <jon.t.parten@nordstrom.com>
2015-06-01 17:37:06 -0700

    idea spec working

--- spec/features/create_idea_spec.rb
+++ spec/features/create_idea_spec.rb
@@ -1,23 +1,28 @@
-require 'rails_helper' 
+require 'spec_helper' 
 
 describe "creating an idea" do 
+
 
   context "with valid parameters" do 
     let(:user) { create(:user) }
 
+    before do
+      login_as(user)
+    end
+
     before do 
       visit "/ideas/new"
-      fill_in "Name", with: "Test Idea 1" 
-      fill_in "Description", with: "Test Idea Description" 
-      fill_in "image", with: "File.open(File.join(Rails.root, 'app/assets/images/default.png'))"
+      fill_in "idea[name]", with: "Test Idea 1" 
+      fill_in "idea[description]", with: "Test Idea Description" 
+      click_button 'Save Idea'
     end 
 
     it "tells the user they have a new idea" do 
-      expect(page).to have_content "Your idea has been successfully created."
+      expect(page).to have_content "Thanks for sharing your idea"
     end 
 
-    it "sends user to the product show page" do 
-      expect(page).to have_content "#{idea.name}" 
+    it "sends user to the idea show page" do 
+      expect(page).to have_content "Test Idea 1" 
     end 
 
   end 
